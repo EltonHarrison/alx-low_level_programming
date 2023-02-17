91839b5fdef58a167ce835dba78e684d463de823
EltonHarrison <okpuelton@gmail.com>
2023-02-16 18:05:46 -0800

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /** 
- *main - this is main function
+ *main - this is main functioin
  *Return: 0 when succesful
  */
 int main(void){
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Return : returns 0 when successful
+ */
+
+int main(void){
+	printf("Size of a char: %lu byte(s)", sizeof(char)):
+	printf("size 0f int: %lu byte(s)", sizeof(int));
+	printf("size of long int : %lu byte(s)", sizeof(long int));
+	printf("size of long long int: %lu byte(s)", sizeof(long long));
+	print("size of a float: %lu byte(s)", sizeof(float);
+	return (0);
+}
