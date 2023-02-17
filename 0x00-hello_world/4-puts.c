a5c62cf9cb2ea4ae28aa1a58d2b0701811754c51
EltonHarrison <okpuelton@gmail.com>
2023-02-16 16:25:53 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  *main - the main function
  *
- *Return - always retuns 0
+ *Return :  always retuns 0
  */
  
 int main(void){
-        puts("\"Programming is like building a multilingual puzzle);
+        puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
