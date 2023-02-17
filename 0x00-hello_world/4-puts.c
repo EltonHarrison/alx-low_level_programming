5563e4741490a9a435d27d0aa115e93d4165066c
EltonHarrison <okpuelton@gmail.com>
2023-02-16 16:13:32 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - the main function
+ *
+ *Return - always retuns 0
+ */
+ 
+int main(void){
+        puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
