bfe93e3d7c98860f076ef13ec4c39e7aa1863e88
EltonHarrison <okpuelton@gmail.com>
2023-02-17 19:53:28 -0800

    Digits

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,9 +6,8 @@
 /**
  * main - main function
  *
- * return : always return 0
+ * Return: always return 0
  */
-
 int main(void)
 {
 	int n;
@@ -16,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)												
+	if (n > 0)	
 		printf("%d is positive", n);
         else if (n == 0)
 	         printf("%d is zero", n); 
