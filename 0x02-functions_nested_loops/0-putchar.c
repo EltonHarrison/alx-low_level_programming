681a651877a17b3870c1b15e2abb0fc5e29d2426
EltonHarrison <okpuelton@gmail.com>
2023-02-21 08:01:42 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - min function 
+ * _putchar: print the alphabets putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
